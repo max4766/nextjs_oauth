c94e2d45f18c88fd9b91510e64a116fbf1494422
hwangyoungsuk <hysgold@eteversepa.com>
2026-01-15 13:25:50 +0900

    deleted console.log for checking session info

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -9,7 +9,6 @@ export default async function ProtectedLayout({
 }>) {
   const session = await getServerSession(authOptions)
   if (!session) redirect("/login")
-  console.log(session);
 
   return (
     <>
